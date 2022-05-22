94fe58243f44dac278293b63dee2512f2ae1bd70
Isabellagomesc <isabella.cardoso@aluno.ifsp.edu.br>
2022-05-22 13:10:06 -0300

    Criação dos arquivos  DatabaseSetup|Computer|ComputerRepository

--- Program.cs
+++ Program.cs
@@ -1,61 +1,95 @@
 ﻿using Microsoft.Data.Sqlite;
+using LabManager.Database;
+using LabManager.Repositories;
 
-var connection = new SqliteConnection("Data Source=database.db"); 
-connection.Open();
+var databaseSetup = new DatabaseSetup();
 
-var command = connection.CreateCommand();
-command.CommandText = @"
-    CREATE TABLE IF NOT EXISTS Computers(
-        id int not null primary key,
-        ram varchar(100) not null,
-        processor varchar(100) not null
-    );
-";
-
-command.ExecuteNonQuery();
-connection.Close();
-
-//Routing 
-
+//Routing
 var modelName = args[0];
 var modelAction = args[1];
 
-if(modelName == "Computer")
+if (modelName == "Computer")
 {
-    if(modelAction == "List")
-    {
-        connection = new SqliteConnection("Data Source=database.db"); 
-        connection.Open();
 
-        command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Computers";
-
-        var reader = command.ExecuteReader();
-
-        while(reader.Read())
+    var computerRepository = new ComputerRepository();
+    
+    if (modelAction == "List")
+    {   
+        Console.WriteLine("Lista de computadores:");
+        
+        foreach (var computer in computerRepository.GetAll())
         {
-            Console.WriteLine("{0}, {1}, {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+            Console.WriteLine($"{computer.Id}, {computer.Ram}, {computer.Processor}");
         }
-
-        connection.Close();
     }
 
-    if(modelAction == "New")
+    else if (modelAction == "New")
     {
         var id = Convert.ToInt32(args[2]);
         var ram = args[3];
         var processor = args[4];
-
-        connection = new SqliteConnection("Data Source=database.db"); 
+        var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
+        var command = connection.CreateCommand();
 
-        command = connection.CreateCommand();
-        command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
+        command.CommandText = $"INSERT INTO Computers VALUES($id, $ram, $processor)";
+
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$ram", ram);
         command.Parameters.AddWithValue("$processor", processor);
 
         command.ExecuteNonQuery();
+
         connection.Close();
+    }
+}
+else if (modelName == "Lab")
+{
+    switch (modelAction)
+    {
+        case "New":
+            {
+                var id = Convert.ToInt32(args[2]);
+                var number = args[3];
+                var name = args[4];
+                var block = args[5];
+                var connection = new SqliteConnection("Data Source=database.db");
+                connection.Open();
+                var command = connection.CreateCommand();
+
+                command.CommandText = $"INSERT INTO Labs VALUES($id, $number, $name, $block)";
+
+                command.Parameters.AddWithValue("$id", id);
+                command.Parameters.AddWithValue("$number", number);
+                command.Parameters.AddWithValue("$name", name);
+                command.Parameters.AddWithValue("$block", block);
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+
+                break;
+            }
+        case "List":
+            {
+                var connection = new SqliteConnection("Data Source=database.db");
+                connection.Open();
+                var command = connection.CreateCommand();
+
+                command.CommandText = $"SELECT * FROM Labs;";
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader.GetInt32(0)} {reader.GetString(1)} {reader.GetString(2)} {reader.GetString(3)}");
+                }
+
+                connection.Close();
+                break;
+            }
+        default:
+            Console.WriteLine("Comando inválido");
+            break;
     }
 }
