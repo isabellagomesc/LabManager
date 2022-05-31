8b451a196718720e2e2992eea5b75c753a122245
isabellagomesc <isabella.cardoso@aluno.ifsp.edu.br>
2022-05-31 20:27:33 -0300

    Adicionado método getbyid

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -4,14 +4,11 @@ using Microsoft.Data.Sqlite;
 
 namespace LabManager.Repositories;
 
-
 class ComputerRepository
 {
-
     private readonly DatabaseConfig _databaseConfig;
 
-    public ComputerRepository(DatabaseConfig databaseConfig)
-    {
+    public ComputerRepository(DatabaseConfig databaseConfig) {
         _databaseConfig = databaseConfig;
     }
 
@@ -19,50 +16,62 @@ class ComputerRepository
     {
         var computers = new List<Computer>();
 
-        var connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
+
         var command = connection.CreateCommand();
-
-        command.CommandText = $"SELECT * FROM Computers;";
-
+        command.CommandText = "SELECT * FROM Computers";
         var reader = command.ExecuteReader();
-
-        while (reader.Read())
+        
+        while(reader.Read())
         {
-            var computer = new Computer(reader.GetInt32(0),reader.GetString(1), reader.GetString(2));
+            var id = reader.GetInt32(0);
+            var ram = reader.GetString(1);
+            var processor = reader.GetString(2);
+            var computer = new Computer(id, ram, processor);
             
             computers.Add(computer);
-
-            /*computer.Add(
-                new Computer(
-                    reader.GetInt32(0),
-                    reader.GetString(1), 
-                    reader.GetString(2)
-                )
-            );*/
         }
-
+        
         connection.Close();
-
+        
         return computers;
     }
 
     public Computer Save(Computer computer)
     {
-            var connection = new SqliteConnection(_databaseConfig.ConnectionString);
-                connection.Open();
-                var command = connection.CreateCommand();
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
 
-                command.CommandText = $"INSERT INTO Labs VALUES($id, $number, $name, $block)";
+        var command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
+        command.Parameters.AddWithValue("$id", computer.Id);
+        command.Parameters.AddWithValue("$ram", computer.Ram);
+        command.Parameters.AddWithValue("$processor", computer.Processor);
 
-                command.Parameters.AddWithValue("$id", computer.Id);
-                command.Parameters.AddWithValue("$ram", computer.Ram);
-                command.Parameters.AddWithValue("$processor", computer.Processor);
+        command.ExecuteNonQuery();
+        connection.Close();
 
-                command.ExecuteNonQuery();
-
-                connection.Close();
-                return computer;
+        return computer;
     }
 
+    public Computer GetById(int id)
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Computers WHERE ID = ($id)";
+        command.Parameters.AddWithValue("$id", id);
+        var reader = command.ExecuteReader();
+
+        reader.Read();
+        var _id = reader.GetInt32(0);
+        var ram = reader.GetString(1);
+        var processor = reader.GetString(2);
+        var computer = new Computer(_id, ram, processor);
+
+        connection.Close();
+        return computer;
+    }
 }
